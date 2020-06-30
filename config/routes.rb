c42357adc925071c1b272ea1d71280a2d001e1f6
Patrik Winkler <winkler.patrik@gmail.com>
2020-06-30 19:30:12 +0200

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, except: [ :edit, :destroy, :update ] do
+  root to: "cocktails#index"
+  resources :cocktails, except: [ :index :edit, :destroy, :update ] do
     resources :doses, only: [ :new, :create ]
   end
   resources :doses, only: [ :destroy ]
